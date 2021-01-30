392822acfc8b48bfe6cb024225a1b11dd997a0f9
Student User <student@workstation.lab.example.com>
2021-01-30 12:39:06 -0500

    Test

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "ALLAH world";
+print "Hello, kutti soothi world" . PHP_VERSION . "\n";
+print "Hello, shaheema kabbu" . PHP_VERSION . "\n";
 ?>
